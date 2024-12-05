fe518892e6b877e6b258e4b94466749d08379a40
Dam Tien Dat <tdat0306@gmail.com>
2024-12-05 22:38:12 +0700

    Remove Test folder and test target in CMakeFileLists.txt

--- Tests/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <windows.h>
-
-int main(int argc, char **argv) {
-    SetConsoleOutputCP(CP_UTF8);
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
